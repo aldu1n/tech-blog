b761c3cb99da62f5105310d322d4dba1beae5232
Bogdan Tkachuk <cnndo14@gmail.com>
2024-02-23 21:52:03 -0500

    added login.js

---
+++ public/js/login.js
@@ -0,0 +1,41 @@
+const loginFromHandler = async (event) => {
+    event.preventDefault(event);
+
+    // const username = document.querySelector('#username-login').value.trim();
+    // const password = document.querySelector('#password-login').value.trim();
+  
+    if (username && password) {
+        const response = await fetch("/api/user/login", {
+          method: "POST",
+          body: JSON.stringify({ username, password }),
+          headers: { "Content-Type": "application/json" },
+        });
+    
+        if (response.ok) {
+          document.location.replace("/");
+        } else {
+          alert(response.statusText);
+        };
+    };
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // const username = document.querySelector('#username-signup').value.trim();
+    // const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
